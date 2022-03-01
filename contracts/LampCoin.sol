60a5313269f3946a353ce077c7bb52625f8345da
Roman Yarlykov <yarlykov92@gmail.com>
2022-03-01 14:21:44 +0700

    moved the LampCoinInterface to a separate file

--- contracts/LampCoin.sol
+++ contracts/LampCoin.sol
@@ -2,18 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-interface LampCoinInterface {
-    function name() external view returns(bytes32);
-    function symbol() external view returns (bytes16);
-    function decimals() external view returns (uint8);
-    function totalSupply() external view returns (uint);
-    function balanceOf(address _owner) external view returns (uint256 balance);
-    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
-
-    function transfer(address _to, uint256 _value) external returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
-    function approve(address _spender, uint256 _value) external returns (bool success);
-}
+import "./LampCoinInterface.sol";
 
 contract LampCoin is LampCoinInterface {
     constructor (uint256 _amount) {
