999c99703b1bdc9da9ca9cb3de88c356a074980a
Roman Yarlykov <yarlykov92@gmail.com>
2022-03-05 12:36:31 +0700

    correct calculation for re-filling the pool

--- contracts/Pair.sol
+++ contracts/Pair.sol
@@ -59,6 +59,10 @@ contract Pair {
       reserve0 = reserve0.add(_amount0);
       reserve1 = reserve1.add(_amount1);
     } else {
+      uint token1Amount = (_amount0 * reserve1) / reserve0;
+      require(_amount1 >= token1Amount, "Insufficient token1 amount");
+      _token0.transferFrom(msg.sender, address(this), _amount0);
+      _token1.transferFrom(msg.sender, address(this), _amount1);
       liquidity = (totalSupply * _amount0) / reserve0;
       this.mint(msg.sender, liquidity);
       reserve0 = reserve0.add(_amount0);
