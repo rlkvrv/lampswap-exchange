3c37a142614ad5e272c74c5c31763215858dbf93
Roman Yarlykov <yarlykov92@gmail.com>
2022-03-14 15:49:22 +0700

    refactoring Pair and Pair tests

--- contracts/Pair.sol
+++ contracts/Pair.sol
@@ -1,121 +1,79 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 import "./libraries/SafeMath.sol";
-import "./LampCoinInterface.sol";
 
-contract Pair {
+contract Pair is ERC20, ReentrancyGuard, Ownable {
     using SafeMath for uint256;
 
-    string public constant name = "LPToken";
-    string public constant symbol = "LPT";
-    uint8 public constant decimals = 18;
-    uint256 public totalSupply;
+    address token0;
+    address token1;
+    address public router;
+    uint256 public reserve0;
+    uint256 public reserve1;
 
-    address private _factory;
-    address public token0;
-    address public token1;
-
-    uint256 private reserve0;
-    uint256 private reserve1;
-
-    constructor(address _token0, address _token1) {
-    require(
-            _token0 != address(0) && _token1 != address(0),
-            "INCORRECT ADDRESS"
-        );
-        _factory = msg.sender;
+    constructor(
+        address _token0,
+        address _token1,
+        address _router
+    ) ERC20("LPToken", "LPT") {
         token0 = _token0;
         token1 = _token1;
+        router = _router;
     }
 
-    mapping(address => mapping(address => uint256)) private allowed;
-    mapping(address => uint256) private coinBalances;
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(
-        address indexed _owner,
-        address indexed _spender,
-        uint256 _value
-    );
-    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
-
-    modifier receiverOverflow(address _to, uint256 _value) {
-        require(
-            coinBalances[_to] + _value >= coinBalances[_to],
-            "Recipient's wallet overflow"
-        );
+    modifier onlyRouter() {
+        require(router == msg.sender, "Ownable: caller is not the router");
         _;
     }
 
-    modifier checkBalance(address _sender, uint256 _value) {
-        require(
-            coinBalances[_sender] >= _value,
-            "Insufficient funds from the sender"
-        );
-        _;
+    function setRouter(address _router) external onlyOwner {
+        router = _router;
     }
 
-    function _getAmount(
-        uint256 inputAmount,
-        uint256 inputReserve,
-        uint256 outputReserve
-    ) private pure returns (uint256) {
-        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
-        uint256 fee = 1;
-        uint256 feeDecimals = 2;
-        uint256 inputAmountWithFee = inputAmount -
-            (inputAmount * fee) /
-            10**feeDecimals;
-        uint256 numerator = outputReserve * inputAmountWithFee;
-        uint256 denominator = inputReserve + inputAmountWithFee;
-        return numerator / denominator;
-    }
-
-    function _mint(address _to, uint256 _amount) private {
-        coinBalances[_to] = coinBalances[_to].add(_amount);
-        totalSupply = totalSupply.add(_amount);
-        emit Transfer(msg.sender, _to, _amount);
-    }
-
-    function _burn(address _from, uint256 _amount) private {
-        coinBalances[_from] = coinBalances[_from].sub(_amount);
-        totalSupply = totalSupply.sub(_amount);
-        emit Transfer(msg.sender, _from, _amount);
-    }
-
-    function createDeposit(uint256 _amount0, uint256 _amount1) external {
-        LampCoinInterface _token0 = LampCoinInterface(token0);
-        LampCoinInterface _token1 = LampCoinInterface(token1);
+    function addLiquidity(
+        address coin0,
+        address coin1,
+        uint256 amount0,
+        uint256 amount1
+    ) external nonReentrant {
+        ERC20 _token0 = ERC20(coin0);
+        ERC20 _token1 = ERC20(coin1);
         uint256 liquidity;
         if (reserve0 == 0) {
-            _token0.transferFrom(msg.sender, address(this), _amount0);
-            _token1.transferFrom(msg.sender, address(this), _amount1);
-            liquidity = _amount0 + _amount1;
+            _token0.transferFrom(msg.sender, address(this), amount0);
+            _token1.transferFrom(msg.sender, address(this), amount1);
+            liquidity = amount0.add(amount1);
             _mint(msg.sender, liquidity);
-            reserve0 = reserve0.add(_amount0);
-            reserve1 = reserve1.add(_amount1);
+            reserve0 = reserve0.add(amount0);
+            reserve1 = reserve1.add(amount1);
         } else {
-            uint256 token0Amount = (_amount1 * reserve0) / reserve1;
-            uint256 token1Amount = (_amount0 * reserve1) / reserve0;
-            require(_amount0 >= token0Amount, "Insufficient token0 amount");
-            require(_amount1 >= token1Amount, "Insufficient token1 amount");
-            _token0.transferFrom(msg.sender, address(this), _amount0);
-            _token1.transferFrom(msg.sender, address(this), _amount1);
-            liquidity = (totalSupply * _amount0) / reserve0;
+            uint256 token0Amount = (amount1.mul(reserve0)).div(reserve1);
+            uint256 token1Amount = (amount0.mul(reserve1)).div(reserve0);
+            require(amount0 >= token0Amount, "Insufficient token0 amount");
+            require(amount1 >= token1Amount, "Insufficient token1 amount");
+            _token0.transferFrom(msg.sender, address(this), amount0);
+            _token1.transferFrom(msg.sender, address(this), amount1);
+            uint256 _totalSupply = this.totalSupply();
+            liquidity = (_totalSupply.mul(amount0)).div(reserve0);
             _mint(msg.sender, liquidity);
-            reserve0 = reserve0.add(_amount0);
-            reserve1 = reserve1.add(_amount1);
+            reserve0 = reserve0.add(amount0);
+            reserve1 = reserve1.add(amount1);
         }
     }
 
-    function removeLiquidity(uint256 _amountLP) external {
+    function removeLiquidity(uint256 _amountLP) external nonReentrant {
         require(_amountLP > 0, "Invalid amount");
-        LampCoinInterface _token0 = LampCoinInterface(token0);
-        LampCoinInterface _token1 = LampCoinInterface(token1);
-        uint256 token0Amount = (reserve0 * _amountLP) / totalSupply;
-        uint256 token1Amount = (reserve1 * _amountLP) / totalSupply;
+        ERC20 _token0 = ERC20(token0);
+        ERC20 _token1 = ERC20(token1);
+        uint256 _totalSupply = this.totalSupply();
+        uint256 token0Amount = (reserve0.mul(_amountLP)).div(_totalSupply);
+        uint256 token1Amount = (reserve1.mul(_amountLP)).div(_totalSupply);
         _burn(msg.sender, _amountLP);
         _token0.transfer(msg.sender, token0Amount);
         _token1.transfer(msg.sender, token1Amount);
@@ -123,13 +81,31 @@ contract Pair {
         reserve1 = reserve1.sub(token1Amount);
     }
 
-    function getReserves()
-        external
-        view
-        returns (uint256 _reserve0, uint256 _reserve1)
-    {
-        _reserve0 = reserve0;
-        _reserve1 = reserve1;
+    function swapIn(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn,
+        address recipient
+    ) external onlyRouter nonReentrant returns (uint256 amountOut) {
+        require(amountIn > 0, "amount too small");
+        require(
+            tokenIn == token0 || tokenIn == token1,
+            "This tokenIn is not found"
+        );
+        require(
+            tokenOut == token0 || tokenOut == token1,
+            "This tokenOut is not found"
+        );
+        amountOut = this.getTokenPrice(tokenIn, amountIn);
+    }
+
+    function swapOut(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountOut,
+        address recipient
+    ) external onlyRouter nonReentrant returns (uint256 amountIn) {
+        amountIn = 0;
     }
 
     function getTokenPrice(address _token, uint256 _amount)
@@ -148,86 +124,19 @@ contract Pair {
         return _getAmount(_amount, reserve1, reserve0);
     }
 
-    function swap(address _token, uint256 _amount) external {
-        require(_amount > 0, "amount too small");
-        require(
-            _token == token0 || _token == token1,
-            "This token is not found"
-        );
-        LampCoinInterface _token0 = LampCoinInterface(token0);
-        LampCoinInterface _token1 = LampCoinInterface(token1);
-        if (_token == token0) {
-            uint256 _token1Bought = _getAmount(_amount, reserve0, reserve1);
-            _token0.transferFrom(msg.sender, address(this), _amount);
-            _token1.transfer(msg.sender, _token1Bought);
-
-            reserve0 = reserve0.add(_amount);
-            reserve1 = reserve1.sub(_token1Bought);
-        } else {
-            uint256 _token0Bought = _getAmount(_amount, reserve1, reserve0);
-            _token1.transferFrom(msg.sender, address(this), _amount);
-            _token0.transfer(msg.sender, _token0Bought);
-
-            reserve1 = reserve1.add(_amount);
-            reserve0 = reserve0.sub(_token0Bought);
-        }
-    }
-
-    function balanceOf(address _owner) external view returns (uint256 balance) {
-        return coinBalances[_owner];
-    }
-
-    function allowance(address _owner, address _spender)
-        external
-        view
-        returns (uint256 remaining)
-    {
-        return allowed[_owner][_spender];
-    }
-
-    function transfer(address _to, uint256 _value)
-        external
-        receiverOverflow(_to, _value)
-        checkBalance(msg.sender, _value)
-        returns (bool success)
-    {
-        coinBalances[msg.sender] -= _value;
-        coinBalances[_to] += _value;
-
-        emit Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _value
-    )
-        external
-        receiverOverflow(_to, _value)
-        checkBalance(_from, _value)
-        returns (bool success)
-    {
-        require(
-            allowed[_from][msg.sender] >= _value,
-            "Ask for permission to transfer the required number of LPT"
-        );
-
-        coinBalances[_from] -= _value;
-        coinBalances[_to] += _value;
-        allowed[_from][msg.sender] -= _value;
-
-        emit Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function approve(address _spender, uint256 _value)
-        external
-        returns (bool success)
-    {
-        allowed[msg.sender][_spender] = _value;
-
-        emit Approval(msg.sender, _spender, _value);
-        return true;
+    function _getAmount(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) private pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+        uint256 fee = 1;
+        uint256 feeDecimals = 2;
+        uint256 inputAmountWithFee = inputAmount -
+            (inputAmount * fee) /
+            10**feeDecimals;
+        uint256 numerator = outputReserve * inputAmountWithFee;
+        uint256 denominator = inputReserve + inputAmountWithFee;
+        return numerator / denominator;
     }
 }
--- test/Pair.test.js
+++ test/Pair.test.js
@@ -4,143 +4,156 @@ const { ethers } = require("hardhat");
 describe("Pair", function () {
   let acc1;
   let acc2;
-  let factory;
-  let token0;
-  let token1;
   let pair;
-  let Pair;
-  let pairContract;
-  let Token;
-  let token0Contract;
-  let token1Contract;
-  let token0TrueAddress;
-  let token1TrueAddress;
+  let acc1Token0;
+  let acc1Token1;
+  let acc2Token0;
+  let acc2Token1;
 
   beforeEach(async function () {
-    [acc1, acc2] = await ethers.getSigners();
-    const Factory = await ethers.getContractFactory('Factory', acc1);
-    factory = await (await Factory.deploy()).deployed();
-    token0 = await (await (await ethers.getContractFactory('LampCoin', acc1))
+    [acc1, acc2, router] = await ethers.getSigners();
+    acc1Token0 = await (await (await ethers.getContractFactory('LampCoin', acc1))
       .deploy(10000))
       .deployed();
-    token1 = await (await (await ethers.getContractFactory('LampCoin', acc1))
+    acc1Token1 = await (await (await ethers.getContractFactory('LampCoin', acc1))
       .deploy(20000))
       .deployed();
-    await factory.createPair(token0.address, token1.address);
-    pair = await factory.getPair(token0.address, token1.address);
-    Pair = require("../artifacts/contracts/Pair.sol/Pair.json");
-    pairContract = new ethers.Contract(pair, Pair.abi, acc1);
-    Token = require("../artifacts/contracts/LampCoin.sol/LampCoin.json");
-    token0TrueAddress = await pairContract.token0();
-    token1TrueAddress = await pairContract.token1();
-    token0Contract = new ethers.Contract(token0TrueAddress, Token.abi, acc1);
-    token1Contract = new ethers.Contract(token1TrueAddress, Token.abi, acc2);
+
+    const Pair = await ethers.getContractFactory("Pair", acc1);
+    pair = await (
+      await Pair.deploy(acc1Token0.address, acc1Token1.address, router.address)
+    ).deployed();
   })
 
-  it("контракт пары должен быть задеплоен", async function () {
-    expect(pairContract.address).to.be.properAddress;
+  it("should be deployed", async function () {
+    expect(pair.address).to.be.properAddress;
   });
 
-  it("totalSupply должен быть равен 0", async function () {
-    const balance = await pairContract.totalSupply();
-    expect(balance).to.be.equal(0);
+  it("name should be LPToken", async function () {
+    expect(await pair.name()).to.be.eq('LPToken');
   });
 
-  describe("createDeposit", async () => {
+  it("decimals should be 18", async function () {
+    expect(await pair.decimals()).to.be.eq(18);
+  });
+
+  it("should be correct router address", async function () {
+    expect(await pair.router()).to.be.eq(router.address);
+  });
+
+
+  describe("addLiquidity", async () => {
     beforeEach(async () => {
-      await token0Contract.connect(acc1).approve(pairContract.address, 100);
-      await token1Contract.connect(acc1).approve(pairContract.address, 200);
+      await acc1Token0.connect(acc1).approve(pair.address, 100);
+      await acc1Token1.connect(acc1).approve(pair.address, 200);
     });
 
-    it("должно быть выдано подтверждение для transferFrom", async () => {
-      expect(await token0Contract.allowance(acc1.address, pairContract.address)).to.be.eq(100);
-      expect(await token1Contract.allowance(acc1.address, pairContract.address)).to.be.eq(200);
+    it("should be approval transfer funds from tokens to pair", async function () {
+      expect(await acc1Token0.allowance(acc1.address, pair.address)).to.be.eq(100);
+      expect(await acc1Token1.allowance(acc1.address, pair.address)).to.be.eq(200);
     });
 
-    it("ликвидность должна добавиться", async () => {
-      await pairContract.connect(acc1).createDeposit(100, 200);
-      expect(await token0Contract.balanceOf(pairContract.address)).to.be.eq(100);
-      expect(await token1Contract.balanceOf(pairContract.address)).to.be.eq(200);
-
-      const reserves = await pairContract.getReserves();
-      expect(reserves[0]).to.be.eq(100);
-      expect(reserves[1]).to.be.eq(200);
+    it("token contracts balance in acc1 should decrease", async function () {
+      await pair.connect(acc1).addLiquidity(acc1Token0.address, acc1Token1.address, 100, 200);
+      expect(await acc1Token0.balanceOf(acc1.address)).to.be.eq(9900);
+      expect(await acc1Token1.balanceOf(acc1.address)).to.be.eq(19800);
     });
 
-    it("должен добавить нулевую ликвидность", async () => {
-      await token0Contract.connect(acc1).approve(pairContract.address, 0);
-      await token1Contract.connect(acc1).approve(pairContract.address, 0);
-      await pairContract.connect(acc1).createDeposit(0, 0);
-
-      const reserves = await pairContract.getReserves();
-      expect(reserves[0]).to.be.eq(0);
-      expect(reserves[1]).to.be.eq(0);
+    it("pair balance should increase", async function () {
+      await pair.connect(acc1).addLiquidity(acc1Token0.address, acc1Token1.address, 100, 200);
+      expect(await acc1Token0.balanceOf(pair.address)).to.be.eq(100);
+      expect(await acc1Token1.balanceOf(pair.address)).to.be.eq(200);
     });
 
-    it("ликвидность должна добавляться в правильном соотношении, иначе ошибка", async () => {
-      await token0Contract.connect(acc1).approve(pairContract.address, 100);
-      await token1Contract.connect(acc1).approve(pairContract.address, 200);
-      await pairContract.connect(acc1).createDeposit(10, 20)
-      await expect(pairContract.connect(acc1).createDeposit(10, 40)).to.be.revertedWith('Insufficient token0 amount')
-      await expect(pairContract.connect(acc1).createDeposit(20, 20)).to.be.revertedWith('Insufficient token1 amount')
-
-      const reserves = await pairContract.connect(acc1).getReserves();
-      expect(reserves[0]).to.be.eq(10);
-      expect(reserves[1]).to.be.eq(20);
+    it("acc1 LPToken balance should increase", async function () {
+      await pair.connect(acc1).addLiquidity(acc1Token0.address, acc1Token1.address, 100, 200);
+      expect(await pair.balanceOf(acc1.address)).to.be.eq(300);
     });
 
-    it("должен сминтить LP tokens (300LP)", async () => {
-      await pairContract.connect(acc1).createDeposit(100, 200);
-      expect(await pairContract.balanceOf(acc1.address)).to.be.eq(300);
-    });
-
-    it("totalSupply должен быть равен 300", async () => {
-      await pairContract.connect(acc1).createDeposit(100, 200);
-      expect(await pairContract.totalSupply()).to.be.eq(300);
+    it("pair totalSupply should increase", async function () {
+      await pair.connect(acc1).addLiquidity(acc1Token0.address, acc1Token1.address, 100, 200);
+      expect(await pair.totalSupply()).to.be.eq(300);
     });
   });
 
-  describe("Tokens swap", async () => {
+  describe("acc2 addLiquidity", async () => {
     beforeEach(async () => {
-      await token0Contract.connect(acc1).approve(pairContract.address, 100);
-      await token1Contract.connect(acc1).approve(pairContract.address, 200);
-      await pairContract.connect(acc1).createDeposit(100, 200);
+      acc2Token0 = await (await (await ethers.getContractFactory('LampCoin', acc2))
+        .deploy(30000))
+        .deployed();
+      acc2Token1 = await (await (await ethers.getContractFactory('LampCoin', acc2))
+        .deploy(40000))
+        .deployed();
+      await acc1Token0.connect(acc1).approve(pair.address, 100);
+      await acc1Token1.connect(acc1).approve(pair.address, 200);
+
+      await acc2Token0.connect(acc2).approve(pair.address, 200);
+      await acc2Token1.connect(acc2).approve(pair.address, 400);
+
+      await pair.connect(acc1).addLiquidity(acc1Token0.address, acc1Token1.address, 100, 200);
     });
 
-
-    it("цена token1 за 10 tokens0 должна равняться 18 token1", async () => {
-      expect(await pairContract.getTokenPrice(token0Contract.address, 10)).to.be.eq(18);
+    it("token contracts balance in acc2 should decrease", async function () {
+      await pair.connect(acc2).addLiquidity(acc2Token0.address, acc2Token1.address, 200, 400);
+      expect(await acc2Token0.balanceOf(acc2.address)).to.be.eq(29800);
+      expect(await acc2Token1.balanceOf(acc2.address)).to.be.eq(39600);
     });
 
-    it("должен обменять token0 на token1", async () => {
-      await token0Contract.connect(acc1).approve(pairContract.address, 10);
-      await pairContract.connect(acc1).swap(token0Contract.address, 10);
+    it("pair balance acc2 should increase", async function () {
+      await pair.connect(acc2).addLiquidity(acc2Token0.address, acc2Token1.address, 200, 400);
+      expect(await acc2Token0.balanceOf(pair.address)).to.be.eq(200);
+      expect(await acc2Token1.balanceOf(pair.address)).to.be.eq(400);
+    });
 
-      expect(await token0Contract.balanceOf(pairContract.address)).to.be.eq(110);
-      expect(await token1Contract.balanceOf(pairContract.address)).to.be.eq(182);
+    it("acc2 LPToken balance should increase", async function () {
+      await pair.connect(acc2).addLiquidity(acc2Token0.address, acc2Token1.address, 200, 400);
+      expect(await pair.balanceOf(acc2.address)).to.be.eq(600);
+    });
 
-      expect(await token0Contract.balanceOf(acc1.address)).to.be.eq(9890);
-      expect(await token1Contract.balanceOf(acc1.address)).to.be.eq(19818);
+    it("pair totalSupply should increase", async function () {
+      await pair.connect(acc2).addLiquidity(acc2Token0.address, acc2Token1.address, 200, 400);
+      expect(await pair.totalSupply()).to.be.eq(900);
+    });
+
+    it("should return error message when added incorrect liquidity", async function () {
+      await expect(pair.connect(acc2).addLiquidity(acc2Token0.address, acc2Token1.address, 199, 400)).to.be.revertedWith('Insufficient token0 amount');
+      await expect(pair.connect(acc2).addLiquidity(acc2Token0.address, acc2Token1.address, 200, 399)).to.be.revertedWith('Insufficient token1 amount');
     });
   });
 
 
   describe("Removed liquidity", async () => {
     beforeEach(async () => {
-      await token0Contract.connect(acc1).approve(pairContract.address, 100);
-      await token1Contract.connect(acc1).approve(pairContract.address, 200);
-      await pairContract.connect(acc1).createDeposit(100, 200);
-      await token0Contract.connect(acc1).approve(pairContract.address, 10);
-      await pairContract.connect(acc1).swap(token0Contract.address, 10);
+      await acc1Token0.connect(acc1).approve(pair.address, 100);
+      await acc1Token1.connect(acc1).approve(pair.address, 200);
+
+      await pair.connect(acc1).addLiquidity(acc1Token0.address, acc1Token1.address, 100, 200);
     });
 
-    it("после удаления ликвидности средства должны вернуться на счет с комиссией, а LP должны быть сожжены", async () => {
-      await pairContract.connect(acc1).removeLiquidity(300);
-      expect(await pairContract.balanceOf(acc1.address)).to.be.eq(0);
-      expect(await token0Contract.balanceOf(pairContract.address)).to.be.eq(0);
-      expect(await token1Contract.balanceOf(pairContract.address)).to.be.eq(0);
-      expect(await token0Contract.balanceOf(acc1.address)).to.be.eq(10000);
-      expect(await token1Contract.balanceOf(acc1.address)).to.be.eq(20000);
+    it("should be removed liquidity acc1", async function () {
+      await pair.connect(acc1).removeLiquidity(300);
+      expect(await acc1Token0.balanceOf(acc1.address)).to.be.eq(10000);
+      expect(await acc1Token1.balanceOf(acc1.address)).to.be.eq(20000);
+    });
+  });
+
+  describe("Tokens swap", async () => {
+    beforeEach(async () => {
+      await acc1Token0.connect(acc1).approve(pair.address, 100);
+      await acc1Token1.connect(acc1).approve(pair.address, 200);
+
+      await pair.connect(acc1).addLiquidity(acc1Token0.address, acc1Token1.address, 100, 200);
+    });
+
+
+    it("price of token1 for 10 tokens0 must be 18 token1", async () => {
+      expect(await pair.getTokenPrice(acc1Token0.address, 10)).to.be.eq(18);
+    });
+
+    it("only router can make a swap transaction", async () => {
+      await expect(
+        pair.connect(acc1).swapIn(acc1Token0.address, acc1Token1.address, 10, acc1.address)
+      ).to.be.revertedWith('Ownable: caller is not the router');
     });
   });
 });
