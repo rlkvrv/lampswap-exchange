2f3654c3ab36797a61dd89688ddc62e121b8cda4
Roman Yarlykov <yarlykov92@gmail.com>
2022-03-15 20:37:30 +0700

    added Fee contract
    - Fee contract
    - FeeInterface interface
    - fee tests

---
+++ contracts/Fee.sol
@@ -0,0 +1,50 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+import "./libraries/SafeMath.sol";
+import "./FeeInterface.sol";
+
+contract Fee is FeeInterface, Ownable {
+    uint256 private swapFee;
+    uint256 private protocolPerformanceFee;
+    uint256 private feeDecimals;
+
+    constructor(
+        uint256 _swapFee,
+        uint256 _protocolPerformanceFee,
+        uint256 _feeDecimals
+    ) {
+        swapFee = _swapFee;
+        protocolPerformanceFee = _protocolPerformanceFee;
+        feeDecimals = _feeDecimals;
+    }
+
+    function setFeeParams(
+        uint256 _swapFee,
+        uint256 _protocolPerformanceFee,
+        uint256 _feeDecimals
+    ) external override onlyOwner {
+        swapFee = _swapFee;
+        protocolPerformanceFee = _protocolPerformanceFee;
+        feeDecimals = _feeDecimals;
+    }
+
+    function getSwapFee() external view override returns (uint256) {
+        return swapFee;
+    }
+
+    function getProtocolPerformanceFee()
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return protocolPerformanceFee;
+    }
+
+    function getFeeDecimals() external view override returns (uint256) {
+        return feeDecimals;
+    }
+}
---
+++ test/Fee.test.js
@@ -0,0 +1,33 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Fee", function () {
+  let acc1;
+  let acc2;
+  let fee;
+
+  beforeEach(async function () {
+    [acc1, acc2] = await ethers.getSigners();
+    const Fee = await ethers.getContractFactory("Fee", acc1);
+    const swapFee = 1;
+    const protocolPerformanceFee = 1;
+    const feeDecimals = 2;
+    fee = await Fee.deploy(swapFee, protocolPerformanceFee, feeDecimals);
+    await fee.deployed();
+  })
+
+  it("should be deployed", async function () {
+    expect(fee.address).to.be.properAddress;
+  });
+
+  it("setFeeParams should set new fee params", async function () {
+    await fee.setFeeParams(2,3,4);
+    expect(await fee.getSwapFee()).to.be.eq(2);
+    expect(await fee.getProtocolPerformanceFee()).to.be.eq(3);
+    expect(await fee.getFeeDecimals()).to.be.eq(4);
+  });
+
+  it("only owner may set params", async function () {
+    await expect(fee.connect(acc2).setFeeParams(2, 3, 4)).to.be.revertedWith('Ownable: caller is not the owner');
+  });
+});
