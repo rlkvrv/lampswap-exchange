2c8d1b6e4534881ed5ce9ef54f9f030353f27e37
Roman Yarlykov <yarlykov92@gmail.com>
2022-03-16 14:19:00 +0700

    added deployed script
    - deployed: Router, Factory, Registry, Fee, token0, token1
    	- set registry address in router
    	- set router address in factory
    	- set registry address in factory
    	- set factory address in registry
    - create pair of two tokens
    	- set router address in pair
    	- set fee adress in pair

--- scripts/deployed.js
+++ scripts/deployed.js
@@ -1,49 +1,60 @@
 const { ethers } = require("hardhat");
 
-async function createTokens(amount, signer) {
-  const LampCoin = await ethers.getContractFactory('LampCoin', signer);
-
-  const ERC20Tokens = [];
-  for (let i = 0; i < amount; i++) {
-    const token = await LampCoin.deploy(10000 * (i + 1));
-    await token.deployed();
-    ERC20Tokens.push(token);
-  }
-
-  return ERC20Tokens;
-}
-
-async function deployFactory() {
-  const Factory = await ethers.getContractFactory('Factory');
-  const factory = await Factory.deploy();
-  await factory.deployed();
-  return factory;
-}
+const DECIMALS = BigInt(10 ** 18);
+const AMOUNT_0 = BigInt(10000n * DECIMALS);
+const AMOUNT_1 = BigInt(20000n * DECIMALS);
 
 async function main() {
-  const [signer] = await ethers.getSigners();
+  const [acc1, acc2] = await ethers.getSigners();
 
-  // создаем массив токенов
-  // сохраняем нужные нам для тестов токены
-  const [token0, token1] = await createTokens(2, signer);
+  // создаем два токена
+  const LampCoin = await ethers.getContractFactory('LampCoin', acc2);
+  const token0 = await (await LampCoin.deploy(AMOUNT_0)).deployed();
+  const token1 = await (await LampCoin.deploy(AMOUNT_1)).deployed();
 
-  // Деплоим фабрику
-  const factory = await deployFactory();
+  // Шаг первый:
+  //  - деплой роутра
+  //  - деплой фабрики
+  //  - деплой регистри
+  //  - деплой контракта с комиссиями
+
+  const Router = await ethers.getContractFactory('Router', acc1);
+  const router = await (await Router.deploy()).deployed();
+
+  const Factory = await ethers.getContractFactory('Factory', acc1);
+  const factory = await (await Factory.deploy()).deployed();
+
+  const Registry = await ethers.getContractFactory("Registry", acc1);
+  const registry = await (await Registry.deploy()).deployed();
+
+  const Fee = await ethers.getContractFactory("Fee", acc1);
+  const fee = await (await Fee.deploy(1, 1, 2)).deployed();
+
+  //Шаг второй:
+  //  - устанавливаем на роутере адрес регистри
+  await router.setRegistry(registry.address);
+  //  - устанавливаем на фабрике адреса регистри и роутера
+  await factory.setRouter(router.address);
+  await factory.setRegistry(registry.address);
+  //  - устанавливаем на регистри адрес фабрики
+  await registry.setFabric(factory.address);
 
   //  Создаем пару через фабрику
   await factory.createPair(token0.address, token1.address);
 
-  // получаем адрес контракта Pair, тут пока порядок адресов не важен
-  const pair1 = await factory.getPair(token0.address, token1.address);
-  console.log('Pair contract address: ', pair1);
+  // получаем адрес контракта Pair
+  const pairAddress = await registry.getPair(token0.address, token1.address)
+  console.log('Pair contract address: ', pairAddress);
 
-  // проверяем количество token0 и token1 на балансах кошелька и Pair
-  console.log('\nstart BALANCE Pair token0: ', await token0.balanceOf(pair1));
-  console.log('start BALANCE Pair token1: ', await token1.balanceOf(pair1));
+  // устанавливаем на паре адреса роутера и физов
+  const Pair = require("../artifacts/contracts/Pair.sol/Pair.json");
+  const pair = new ethers.Contract(pairAddress, Pair.abi, acc1);
+  await pair.setRouter(router.address);
+  await pair.setFee(fee.address);
 
-  // Делаем проверку, что средства списались с кошелька
-  console.log('\nstart BALANCE myWallet token0: ', await token0.balanceOf(signer.address));
-  console.log('start BALANCE myWallet1 token1: ', await token1.balanceOf(signer.address));
+  // Проверяем баланс токенов на acc1
+  console.log('\nstart BALANCE myWallet token0: ', await token0.balanceOf(acc2.address));
+  console.log('start BALANCE myWallet1 token1: ', await token1.balanceOf(acc2.address));
 }
 
 main()
