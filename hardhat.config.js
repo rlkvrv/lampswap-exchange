3b77d0299a3fb727fe72ee3dcd1faa3564ed3ba4
Roman Yarlykov <roman.yarlykov@metalamp.io>
2022-05-13 19:22:39 +0700

    added optimazer settings

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,31 +1,39 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
-require('solidity-coverage');
+require("solidity-coverage");
 require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 module.exports = {
-  solidity: "0.8.10",
-  networks: {
-    hardhat: {
-      chainId: 1337
+    solidity: {
+        version: "0.8.10",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000,
+            },
+        },
     },
-    rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
-      accounts: [process.env.PRIVATE_KEY]
+    networks: {
+        hardhat: {
+            chainId: 1337
+        },
+        rinkeby: {
+            url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+            accounts: [process.env.PRIVATE_KEY]
+        },
     },
-  },
-  etherscan: {
-    apiKey: `${process.env.ETHERSCAN_KEY}`,
-  },
+    etherscan: {
+        apiKey: `${process.env.ETHERSCAN_KEY}`,
+    },
 };
